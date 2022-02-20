0633806f6def6fbd3acf982cf48a95d93085f1cf
potados99 <potados99@gmail.com>
2022-02-21 05:20:27 +0900

    chore: / 엔드포인트 수정

--- lib/express/routes/root.ts
+++ lib/express/routes/root.ts
@@ -2,10 +2,13 @@ import express from 'express';
 import oracledb from 'oracledb';
 
 export default express.Router().get('/', async (req, res) => {
-  const pool = oracledb.getPool();
-  const connection = await pool.getConnection();
-  await connection.execute('SELECT 1');
-  await connection.release();
+  try {
+    const connection = await oracledb.getPool().getConnection();
+    await connection.execute('SELECT 1');
+    await connection.release();
 
-  return res.send('서버도 DB도 살아있음!');
+    return res.send('서버도 DB도 살아있음!');
+  } catch (e) {
+    return res.send(`서버는 살아있으나 DB가 죽음: ${e}`);
+  }
 });
