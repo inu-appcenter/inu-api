205efc9f5d4e4f4b1adef65abf61517111e90a46
potados99 <potados99@gmail.com>
2022-02-22 00:30:47 +0900

    refactor: Buffer.alloc 사용

--- lib/common/utils/cipher.ts
+++ lib/common/utils/cipher.ts
@@ -2,7 +2,7 @@ import crypto from 'crypto';
 
 export function decrypt(encrypted: string, secret: string) {
   const key = crypto.scryptSync(secret, 'yeah', 32);
-  const iv = new Buffer(16);
+  const iv = Buffer.alloc(16);
 
   const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
   decipher.update(encrypted, 'hex', 'utf-8');
