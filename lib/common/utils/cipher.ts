fc5ed7445630ec35563d94f3b9c7c717d1b0d900
potados99 <potados99@gmail.com>
2022-02-22 00:26:05 +0900

    fix: #1

--- lib/common/utils/cipher.ts
+++ lib/common/utils/cipher.ts
@@ -1,9 +1,11 @@
 import crypto from 'crypto';
 
-export function decrypt(encrypted: string, key: string) {
-  const iv = crypto.randomBytes(8).toString('hex');
+export function decrypt(encrypted: string, secret: string) {
+  const key = crypto.scryptSync(secret, 'yeah', 32);
+  const iv = new Buffer(16);
+
   const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
-  decipher.update(encrypted, 'base64', 'utf-8');
+  decipher.update(encrypted, 'hex', 'utf-8');
 
   return decipher.final('utf-8');
 }
